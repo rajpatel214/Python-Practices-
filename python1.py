3d7039edd4131bbbd3b91b41f1a4579628edf320
Raj Patel <rajpatel.d.1012220@gmail.com>
2025-05-08 06:37:08 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,22 @@
+
+
+#Task 1: Perform Basic Mathematical Operations
+
+
+# num1= float(input("enter yor number1: "))
+# num2= float(input("enter yor number2: "))
+
+
+# print("Addition :",num1 + num2 )
+# print("Subtraction :", num1-num2 )
+# print("Multiplication :", num1*num2 )
+# print("Division :", num1/num2 )
+
+
+#Task 2: Create a Personalized Greeting
+
+first_name = input("Enter Your First Name: ")
+last_name = input("Enter Your Last Name: ")
+
+welcome = f"Hello, {first_name} {last_name}! Welcome to the Python program."
+print(welcome)
